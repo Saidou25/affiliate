3bc116180e8236d7d9e45bfc3f53e10de940025e
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2025-04-29 03:55:57 -0400

    installing render

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/', // make sure this matches your server
+  uri: import.meta.env.VITE_API_BASE_URL,
   cache: new InMemoryCache(),
 });
 
