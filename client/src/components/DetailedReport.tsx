db420913c29060897acd751e3efce8c165d95318
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2025-06-04 15:00:12 -0400

    fixed Affiliate type

--- client/src/components/DetailedReport.tsx
+++ client/src/components/DetailedReport.tsx
@@ -16,6 +16,7 @@ interface AffiliateSale {
   buyerEmail: string;
   event: string;
   commissionEarned: number;
+  commissionStatus: string;
   timestamp: string;
   amount: number;
   productId: string;
